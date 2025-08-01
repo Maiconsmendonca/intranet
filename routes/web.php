397abbdd9d90a4945006584fbd7ebb22b2591d48
Maicon Mendonca <maicon.mendonca@hotmail.com>
2025-08-01 15:51:52 -0300

    update routes with lms routes in inglish

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,30 @@ Route::middleware(['auth', 'verified'])->group(function () {
 Route::prefix('lms')->middleware(['auth'])->group(function () {
 
     // Cursos
-    Route::get('/cursos', [CursoController::class, 'index']);        // Listar cursos
-    Route::post('/cursos', [CursoController::class, 'store']);       // Criar novo curso
-    Route::get('/cursos/{id}', [CursoController::class, 'show']);    // Ver curso específico
-    Route::put('/cursos/{id}', [CursoController::class, 'update']);  // Atualizar curso
-    Route::delete('/cursos/{id}', [CursoController::class, 'destroy']); // Deletar curso
+    Route::get('/cursos', [CourseController::class, 'index']);        // Listar cursos
+    Route::post('/cursos', [CourseController::class, 'store']);       // Criar novo curso
+    Route::get('/cursos/{id}', [CourseController::class, 'show']);    // Ver curso específico
+    Route::put('/cursos/{id}', [CourseController::class, 'update']);  // Atualizar curso
+    Route::delete('/cursos/{id}', [CourseController::class, 'destroy']); // Deletar curso
 
     // Módulos (dentro de um curso)
-    Route::post('/cursos/{id}/modulos', [ModuloController::class, 'store']);
-    Route::get('/modulos/{id}', [ModuloController::class, 'show']);
+    Route::post('/cursos/{id}/modulos', [ModuleController::class, 'store']);
+    Route::get('/modulos/{id}', [ModuleController::class, 'show']);
 
     // Aulas (dentro de um módulo)
-    Route::post('/modulos/{id}/aulas', [AulaController::class, 'store']);
-    Route::get('/aulas/{id}', [AulaController::class, 'show']);
-    Route::post('/aulas/{id}/assistir', [AulaController::class, 'marcarComoAssistida']);
+    Route::post('/modulos/{id}/aulas', [LessonController::class, 'store']);
+    Route::get('/aulas/{id}', [LessonController::class, 'show']);
+    Route::post('/aulas/{id}/assistir', [LessonController::class, 'marcarComoAssistida']);
 
     // Avaliações
-    Route::post('/aulas/{id}/avaliacoes', [AvaliacaoController::class, 'store']);
-    Route::post('/avaliacoes/{id}/responder', [AvaliacaoController::class, 'responder']);
+    Route::post('/aulas/{id}/avaliacoes', [QuizController::class, 'store']);
+    Route::post('/avaliacoes/{id}/responder', [QuizController::class, 'responder']);
 
     // Progresso do aluno
-    Route::get('/meu-progresso', [ProgressoController::class, 'index']);
+    Route::get('/meu-progresso', [ProgressController::class, 'index']);
 
     // Certificados
-    Route::get('/cursos/{id}/certificado', [CertificadoController::class, 'gerar']);
+    Route::get('/cursos/{id}/certificado', [CertificationController::class, 'gerar']);
 });
 
 require __DIR__.'/settings.php';
